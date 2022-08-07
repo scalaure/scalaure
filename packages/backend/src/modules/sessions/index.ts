72d898fa1b18de5aea39ecc792389f06e1940e75
adipol1359 <adrian.polak20@gmail.com>
2022-08-07 12:33:52 +0200

    refactor: change endpoint

--- packages/backend/src/modules/sessions/index.ts
+++ packages/backend/src/modules/sessions/index.ts
@@ -3,7 +3,7 @@ import { createSession } from './sessions.handlers';
 import type { FastifyPluginCallbackTypebox } from '@fastify/type-provider-typebox';
 
 const sessionsPlugin: FastifyPluginCallbackTypebox = (fastify, options, done) => {
-  fastify.post('/me', { schema: createSessionSchema }, createSession);
+  fastify.post('/', { schema: createSessionSchema }, createSession);
 
   done();
 };
