55959a0f03880571b9dbee09a63d5b9fd67ee145
adipol1359 <adrian.polak20@gmail.com>
2022-08-07 13:07:02 +0200

    refactor: change return way

--- packages/backend/src/utils/typeguards.ts
+++ packages/backend/src/utils/typeguards.ts
@@ -1,9 +1,5 @@
 import { Prisma } from '@prisma/client';
 import type { PrismaErrors } from './prismaErrors';
 
-export const isPrismaError = (err: unknown): err is PrismaErrors => {
-  if (err && err instanceof Prisma.PrismaClientKnownRequestError) {
-    return true;
-  }
-  return false;
-};
+export const isPrismaError = (err: unknown): err is PrismaErrors =>
+  !!err && err instanceof Prisma.PrismaClientKnownRequestError;
