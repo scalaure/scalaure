0ed9fb2bd151e0215db46d9f5be6ec467537b7f6
adipol1359 <adrian.polak20@gmail.com>
2022-08-07 12:57:28 +0200

    refactor: change parameter type

--- packages/backend/src/plugins/users.ts
+++ packages/backend/src/plugins/users.ts
@@ -1,18 +1,15 @@
 import fp from 'fastify-plugin';
-import type { User } from '@prisma/client';
+import type { Prisma, User } from '@prisma/client';
 import type { FastifyPluginCallback } from 'fastify';
 
 declare module 'fastify' {
   interface FastifyInstance {
-    readonly findUserBy: (
-      where: { readonly email?: string; readonly id?: number },
-      details?: boolean
-    ) => Promise<User | null>;
+    readonly findUserBy: (where: Prisma.UserWhereInput, details?: boolean) => Promise<User | null>;
   }
 }
 
 const usersPlugin: FastifyPluginCallback = (fastify, _options, done) => {
-  fastify.decorate('findUserBy', async (where: { readonly email?: string; readonly id?: number }, details = false) => {
+  fastify.decorate('findUserBy', async (where: Prisma.UserWhereInput, details = false) => {
     const user = await fastify.prisma.user.findFirst({ where, include: { details } });
     return user;
   });
