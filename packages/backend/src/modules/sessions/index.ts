d3066100e040d6d29449faa22bb427d463589d00
AdiPol1359 <adrian.polak20@gmail.com>
2022-08-09 14:35:36 +0200

    add /sessions/me endpoint

--- packages/backend/src/modules/sessions/index.ts
+++ packages/backend/src/modules/sessions/index.ts
@@ -1,9 +1,10 @@
-import { createSessionSchema } from '@scalaure/common';
-import { createSession } from './sessions.handlers';
+import { createSessionSchema, getSessionSchema } from '@scalaure/common';
+import { createSession, getSession } from './sessions.handlers';
 import type { FastifyPluginCallbackTypebox } from '@fastify/type-provider-typebox';
 
 const sessionsPlugin: FastifyPluginCallbackTypebox = (fastify, _options, done) => {
   fastify.post('/', { schema: createSessionSchema }, createSession);
+  fastify.get('/me', { schema: getSessionSchema, preHandler: fastify.auth() }, getSession);
 
   done();
 };
