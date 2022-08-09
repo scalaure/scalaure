b5abd4dccaa82fa8cf3fac5bea3d27e8bc876ce3
AdiPol1359 <adrian.polak20@gmail.com>
2022-08-09 14:39:04 +0200

    feat: add getSessionHandler & change return body from POST /sessions

--- packages/backend/src/modules/sessions/sessions.handlers.ts
+++ packages/backend/src/modules/sessions/sessions.handlers.ts
@@ -1,12 +1,19 @@
 import bcrypt from 'bcrypt';
-import type { createSessionSchema } from '@scalaure/common';
+import type { createSessionSchema, getSessionSchema } from '@scalaure/common';
 import type { TypeBoxRouteHandlerMethod } from 'types';
 
 export const createSession: TypeBoxRouteHandlerMethod<typeof createSessionSchema> = async (request, reply) => {
-  const { findUserBy } = request.server;
+  const { prisma } = request.server;
   const { email, password } = request.body;
 
-  const user = await findUserBy({ email });
+  if (request.session.userId) {
+    return reply.notAcceptable('You are already logged in.');
+  }
+
+  const user = await prisma.user.findFirst({
+    where: { email },
+    include: { details: true }
+  });
 
   if (!user || !(await bcrypt.compare(password, user.password))) {
     return reply.notFound('Incorrect username or password.');
@@ -18,5 +25,9 @@ export const createSession: TypeBoxRouteHandlerMethod<typeof createSessionSchema
 
   request.session.userId = user.id;
 
-  return { loggedin: true }; // TODO: change response body
+  return user;
+};
+
+export const getSession: TypeBoxRouteHandlerMethod<typeof getSessionSchema> = async request => {
+  return request.userData;
 };
