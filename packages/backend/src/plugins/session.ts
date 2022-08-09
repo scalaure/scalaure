e6bacd03202ad87db398b004f9e38973d1194287
AdiPol1359 <adrian.polak20@gmail.com>
2022-08-09 12:09:19 +0200

    feat: add auth decorator

--- packages/backend/src/plugins/session.ts
+++ packages/backend/src/plugins/session.ts
@@ -1,8 +1,17 @@
 import fastifySession from '@fastify/session';
 import fp from 'fastify-plugin';
-import type { FastifyPluginAsync } from 'fastify';
+import type { FastifyPluginAsync, preHandlerAsyncHookHandler } from 'fastify';
+import type { UserRole, User, UserDetails } from '@prisma/client';
 
 declare module 'fastify' {
+  interface FastifyInstance {
+    auth: (...roles: UserRole[]) => preHandlerAsyncHookHandler;
+  }
+
+  interface FastifyRequest {
+    userData: User & { details: UserDetails };
+  }
+
   interface Session {
     // eslint-disable-next-line -- need mutable property here
     userId: number;
@@ -16,6 +25,28 @@ const sessionPlugin: FastifyPluginAsync = async fastify => {
     secret: config.SESSION_SECRET,
     cookieName: config.SESSION_COOKIE_NAME,
     cookie: { secure: process.env.NODE_ENV === 'production' }
+  });
+
+  fastify.decorate('auth', (...roles: UserRole[]): preHandlerAsyncHookHandler => {
+    return async (request, reply) => {
+      const { userId } = request.session;
+      const { prisma } = request.server;
+
+      if (!userId) {
+        return reply.unauthorized('Unauthorized.');
+      }
+
+      const user = await prisma.user.findFirstOrThrow({
+        where: { id: userId },
+        include: { details: true }
+      });
+
+      if (!roles.some(role => user.roles.includes(role))) {
+        return reply.forbidden('Forbidden.');
+      }
+
+      request.userData = user;
+    };
   });
 };
 
