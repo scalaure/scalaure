c1b833eca58a4d5576f53d6baa498cb85f265cd9
AdiPol1359 <adrian.polak20@gmail.com>
2022-08-09 14:34:43 +0200

    feat: add getSessionSchema

--- packages/common/src/modules/sessions/schemas.ts
+++ packages/common/src/modules/sessions/schemas.ts
@@ -1,9 +1,19 @@
 import { Type } from '@sinclair/typebox';
 import { createTypeBoxFastifySchema } from '../../utils/schema';
+import { UserSchema } from '../users';
 
 export const createSessionSchema = createTypeBoxFastifySchema({
   body: Type.Object({
     email: Type.String(),
     password: Type.String()
-  })
+  }),
+  response: {
+    200: UserSchema
+  }
+});
+
+export const getSessionSchema = createTypeBoxFastifySchema({
+  response: {
+    200: UserSchema
+  }
 });
